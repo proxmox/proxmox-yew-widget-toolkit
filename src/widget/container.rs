b89cfcd2ac80bbbf2abc3fad8267ea6e94fb6827
Dietmar Maurer <dietmar@proxmox.com>
2022-06-24 09:21:58 +0200

    Container: add tag property

--- src/widget/container.rs
+++ src/widget/container.rs
@@ -1,16 +1,30 @@
 use std::borrow::Cow;
 
+use yew::prelude::*;
 use yew::virtual_dom::{Listeners, VList, VTag};
 
 use pwt_macros::widget;
 
 #[widget(@element, @container)]
-#[derive(Default, Debug, Clone)]
-pub struct Container {}
+#[derive(Default, Debug, Clone, PartialEq, Properties)]
+pub struct Container {
+    #[prop_or(Cow::Borrowed("div"))]
+    tag: Cow<'static, str>,
+}
 
 impl Container {
     pub fn new() -> Self {
-        Self::default()
+        yew::props!{ Self {
+        }}
+    }
+
+    pub fn tag(mut self, tag: impl Into<Cow<'static, str>>) -> Self {
+        self.set_tag(tag);
+        self
+    }
+
+    pub fn set_tag(&mut self, tag: impl Into<Cow<'static, str>>) {
+        self.tag = tag.into();
     }
 }
 
@@ -25,8 +39,9 @@ impl Into<VTag> for Container {
 
         let children = VList::with_children(self.children, None);
 
+
         VTag::__new_other(
-            Cow::Borrowed("div"),
+            self.tag,
             self.std_props.node_ref,
             self.std_props.key,
             attributes,
