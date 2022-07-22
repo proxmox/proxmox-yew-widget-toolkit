6f7726dfa342025e8b574b86c65b80ad2d8e4524
Dominik Csapak <d.csapak@proxmox.com>
2022-07-22 08:48:57 +0200

    component/mod.rs: rustfmt
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -1,11 +1,11 @@
 mod alert_dialog;
-pub use alert_dialog::{error_message, display_load_result, AlertDialog, PwtAlertDialog};
+pub use alert_dialog::{display_load_result, error_message, AlertDialog, PwtAlertDialog};
 
 mod edit_window;
 pub use edit_window::{EditWindow, PbsEditWindow};
 
 mod key_value_grid;
-pub use key_value_grid::{KVGrid, PwtKVGrid, KVGridRow};
+pub use key_value_grid::{KVGrid, KVGridRow, PwtKVGrid};
 
 mod object_grid;
-pub use object_grid::{ObjectGrid, PwtObjectGrid, ObjectGridRow};
+pub use object_grid::{ObjectGrid, ObjectGridRow, PwtObjectGrid};
