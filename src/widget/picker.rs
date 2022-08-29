3a708a7219bff8ddc8dac5225a2fe39b405f0e81
Dietmar Maurer <dietmar@proxmox.com>
2022-08-29 13:48:54 +0200

    GridPicker: impl. Component
    I want to add filter state later...

--- src/widget/picker.rs
+++ src/widget/picker.rs
@@ -1,9 +1,10 @@
 use std::rc::Rc;
-use derivative::Derivative;
+use std::marker::PhantomData;
 
+use derivative::Derivative;
 use yew::prelude::*;
-use yew::html::IntoEventCallback;
-use yew::virtual_dom::{Key, VNode};
+use yew::html::{IntoEventCallback, IntoPropValue};
+use yew::virtual_dom::{Key, VComp, VNode};
 
 use crate::widget::DataTableColumn;
 use crate::widget::focus::focus_next_tabable;
@@ -18,6 +19,7 @@ where
 {
     #[prop_or_default]
     node_ref: NodeRef,
+    pub key: Option<Key>,
 
     pub columns: Vec<DataTableColumn<T>>,
 
@@ -37,8 +39,31 @@ where
 
 impl<T> GridPicker<T> {
 
+    // Create a new instance.
     pub fn new(columns: Vec<DataTableColumn<T>>) -> Self {
         yew::props!(GridPicker<T> { columns })
+    }
+
+    /// Builder style method to set the yew `node_ref`
+    pub fn node_ref(mut self, node_ref: ::yew::html::NodeRef) -> Self {
+        self.set_node_ref(node_ref);
+        self
+    }
+
+    /// Method to set the yew `node_ref`
+    pub fn set_node_ref(&mut self, node_ref: ::yew::html::NodeRef) {
+        self.node_ref = node_ref;
+    }
+
+    /// Builder style method to set the yew `key` property
+    pub fn key(mut self, key: impl IntoPropValue<Option<Key>>) -> Self {
+        self.set_key(key);
+        self
+    }
+
+    /// Method to set the yew `key` property
+    pub fn set_key(&mut self, key: impl IntoPropValue<Option<Key>>) {
+        self.key = key.into_prop_value();
     }
 
     pub fn data(mut self, data: Rc<Vec<T>>) -> Self {
@@ -69,22 +94,39 @@ impl<T> GridPicker<T> {
         self.show_header = show_header;
         self
     }
+}
 
-    pub fn html(&self) -> Html {
+#[doc(hidden)]
+pub struct PwtGridPicker<T> {
+    _phantom: PhantomData<T>,
+}
 
-        let headers: Html = self.columns.iter().map(|column| {
+impl<T: 'static> Component for PwtGridPicker<T> {
+    type Message = ();
+    type Properties = GridPicker<T>;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            _phantom: PhantomData::<T>,
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let props = ctx.props();
+
+        let headers: Html = props.columns.iter().map(|column| {
             html!{<th>{column.name.clone()}</th>}
         }).collect();
 
-        let is_list = self.columns.len() == 1; // Simple listbox or grid ?
+        let is_list = props.columns.len() == 1; // Simple listbox or grid ?
 
-        let options: Html = self.items.iter().enumerate().map(|(n, item)| {
-            let selected = self.selection.map(|sel| sel == n).unwrap_or(false);
+        let options: Html = props.items.iter().enumerate().map(|(n, item)| {
+            let selected = props.selection.map(|sel| sel == n).unwrap_or(false);
             let tabindex = if selected { "0" } else { "-1" };
             let class = classes!(selected.then(|| "selected"));
             let cell_class = String::from("pwt-text-truncate");
 
-            let cells: Html = self.columns.iter().enumerate().map(|(n, column)| {
+            let cells: Html = props.columns.iter().enumerate().map(|(n, column)| {
                 let item_style = format!("justify-content:{}; grid-column:{};", column.justify, n+1);
                 let class = if selected { Some("selected") } else {None };
 
@@ -93,13 +135,13 @@ impl<T> GridPicker<T> {
                 }
             }).collect();
 
-            let key = match &self.extract_key {
+            let key = match &props.extract_key {
                 None => Key::from(n),
                 Some(extract_fn) => extract_fn.apply(item),
             };
 
             let onclick = Callback::from({
-                let onselect = self.onselect.clone();
+                let onselect = props.onselect.clone();
                 let value = key.clone();
                 move |_| {
                     if let Some(onselect) = &onselect {
@@ -109,7 +151,7 @@ impl<T> GridPicker<T> {
             });
 
             let onkeydown = Callback::from({
-                let onselect = self.onselect.clone();
+                let onselect = props.onselect.clone();
                 let value = key.clone();
                 move |event: KeyboardEvent| {
                     match event.key_code() {
@@ -138,7 +180,7 @@ impl<T> GridPicker<T> {
         }).collect();
 
         let onkeydown = Callback::from({
-            let node_ref = self.node_ref.clone();
+            let node_ref = props.node_ref.clone();
             move |event: KeyboardEvent| {
                 match event.key_code() {
                     40 => { // down
@@ -154,16 +196,18 @@ impl<T> GridPicker<T> {
         });
 
         html! {
-            <table role={if is_list { "listbox" } else {"grid"}} ref={self.node_ref.clone()} class="pwt-table pwt-fit table-hover table-striped pwt-border" {onkeydown}>
-                if self.show_header { <thead><tr>{headers}</tr></thead> }
+            <table role={if is_list { "listbox" } else {"grid"}} ref={props.node_ref.clone()} class="pwt-table pwt-fit table-hover table-striped pwt-border" {onkeydown}>
+                if props.show_header { <thead><tr>{headers}</tr></thead> }
                 <tbody>{options}</tbody>
             </table>
         }
     }
 }
 
-impl<T> Into<VNode> for GridPicker<T> {
+impl<T: 'static> Into<VNode> for GridPicker<T> {
     fn into(self) -> VNode {
-        self.html()
+        let key = self.key.clone();
+        let comp = VComp::new::<PwtGridPicker<T>>(Rc::new(self), key);
+        VNode::from(comp)
     }
 }
