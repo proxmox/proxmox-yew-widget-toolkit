ae9beb016685d6ada1ace1ccec4aeea43fb59798
Dietmar Maurer <dietmar@proxmox.com>
2023-01-15 12:29:27 +0100

    improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -57,6 +57,7 @@
 //! - [widget::Column]: Vertical container with flex layout.
 //! - [widget::Panel]: Container with title.
 //! - [widget::InputPanel]: Container to create simple forms.
+//! - [widget::SplitPane]: Container where children are separated by a draggable sparator.
 //! - [widget::TabPanel]: A set of layered items where only one item is displayed at a time.
 //! - [widget::Toolbar]: Horizontal container for buttons.
 //!
