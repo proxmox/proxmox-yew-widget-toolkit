0afe5c91217f0895f7671f4b54596ef3425260b0
Dietmar Maurer <dietmar@proxmox.com>
2023-01-19 12:59:52 +0100

    add more border classes.

--- src/props/border.rs
+++ src/props/border.rs
@@ -12,6 +12,10 @@ use super:: AsClassesMut;
 /// - `pwt-no-border-top`: no border on top.
 /// - `pwt-border-bottom`: border on bottom.
 /// - `pwt-no border-bottom`: no border on bottom.
+/// - `pwt-border-start`: border on the start.
+/// - `pwt-no-border-start`: no border on the start.
+/// - `pwt-border-end`: border on the end.
+/// - `pwt-no-border-end`: border on the end.
 /// - `pwt-border-left`: border on the left side.
 /// - `pwt-no-border-left`: no border on the left side.
 /// - `pwt-border-right`: border on the right side.
@@ -75,14 +79,42 @@ pub trait CssBorderBuilder: AsClassesMut + Sized {
         };
     }
 
-    /// Builder style method to add a left border class.
+    /// Builder style method to add a start border class.
     fn border_start(mut self, border: impl IntoPropValue<Option<bool>>) -> Self {
         self.add_border_start(border);
         self
     }
 
-    /// Method to add a left border class.
+    /// Method to add a start border class.
     fn add_border_start(&mut self, border: impl IntoPropValue<Option<bool>>) {
+         let border = border.into_prop_value();
+        if let Some(border) = border {
+            self.as_classes_mut().push(if border { "pwt-border-start" } else { "pwt-no-border-start" });
+        };
+    }
+
+    /// Builder style method to add an end border class.
+    fn border_end(mut self, border: impl IntoPropValue<Option<bool>>) -> Self {
+        self.add_border_end(border);
+        self
+    }
+
+    /// Method to add an end border class.
+    fn add_border_end(&mut self, border: impl IntoPropValue<Option<bool>>) {
+        let border = border.into_prop_value();
+        if let Some(border) = border {
+            self.as_classes_mut().push(if border { "pwt-border-end" } else { "pwt-no-border-end" });
+        };
+    }
+
+    /// Builder style method to add a left border class.
+    fn border_left(mut self, border: impl IntoPropValue<Option<bool>>) -> Self {
+        self.add_border_left(border);
+        self
+    }
+
+    /// Method to add a left border class.
+    fn add_border_left(&mut self, border: impl IntoPropValue<Option<bool>>) {
          let border = border.into_prop_value();
         if let Some(border) = border {
             self.as_classes_mut().push(if border { "pwt-border-left" } else { "pwt-no-border-left" });
@@ -90,13 +122,13 @@ pub trait CssBorderBuilder: AsClassesMut + Sized {
     }
 
     /// Builder style method to add a right border class.
-    fn border_end(mut self, border: impl IntoPropValue<Option<bool>>) -> Self {
-        self.add_border_end(border);
+    fn border_right(mut self, border: impl IntoPropValue<Option<bool>>) -> Self {
+        self.add_border_right(border);
         self
     }
 
     /// Method to add a right border class.
-    fn add_border_end(&mut self, border: impl IntoPropValue<Option<bool>>) {
+    fn add_border_right(&mut self, border: impl IntoPropValue<Option<bool>>) {
         let border = border.into_prop_value();
         if let Some(border) = border {
             self.as_classes_mut().push(if border { "pwt-border-right" } else { "pwt-no-border-right" });
