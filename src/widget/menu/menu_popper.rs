a7eb62f9d14461cb896df6334895f256fe36a6f2
Dominik Csapak <d.csapak@proxmox.com>
2023-01-24 11:12:38 +0100

    widget: menu_popper: change to our align_to
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- src/widget/menu/menu_popper.rs
+++ src/widget/menu/menu_popper.rs
@@ -1,62 +1,58 @@
-use wasm_bindgen::JsValue;
-use serde_json::json;
-
 use yew::prelude::*;
+
+use crate::widget::align::{AlignOptions, GrowDirection, Point};
 
 pub struct MenuPopper {
     content_ref: NodeRef,
     submenu_ref: NodeRef,
-    popper: Option<JsValue>,
+    align_options: Option<AlignOptions>,
     prefer_bottom: bool,
 }
 
 impl MenuPopper {
-
     pub fn new(content_ref: NodeRef, submenu_ref: NodeRef, prefer_bottom: bool) -> Self {
         Self {
-            popper: None,
             content_ref,
             submenu_ref,
             prefer_bottom,
+            align_options: None,
         }
     }
 
     pub fn update(&mut self) {
-        if self.popper.is_none() {
-            let opts = json!({
-                "placement": if self.prefer_bottom { "bottom-start" } else { "right-start"},
-                "strategy": "fixed",
-                "modifiers": [
-                    {
-                        "name": "preventOverflow",
-                        "options": {
-                            "mainAxis": true, // true by default
-                            "altAxis": false, // false by default
-                        },
-                    },
-                    {
-                        "name": "flip",
-                        "options": {
-                            "fallbackPlacements": [
-                                if self.prefer_bottom { "right-start" } else { "bottom-start" },
-                                "left-start",
-                            ],
-                        },
-                    },
-                ],
+        if self.align_options.is_none() {
+            self.align_options = Some(if self.prefer_bottom {
+                AlignOptions::new(Point::BottomStart, Point::TopStart, GrowDirection::StartEnd)
+                    .align_width(true)
+                    .viewport_padding(5.0)
+                    .with_fallback_placement(
+                        Point::TopEnd,
+                        Point::TopStart,
+                        GrowDirection::TopBottom,
+                    )
+                    .with_fallback_placement(
+                        Point::TopStart,
+                        Point::TopEnd,
+                        GrowDirection::TopBottom,
+                    )
+            } else {
+                AlignOptions::new(Point::TopEnd, Point::TopStart, GrowDirection::TopBottom)
+                    .viewport_padding(5.0)
+                    .align_width(true)
+                    .with_fallback_placement(
+                        Point::TopStart,
+                        Point::TopEnd,
+                        GrowDirection::TopBottom,
+                    )
             });
-
-            let opts = crate::to_js_value(&opts).unwrap();
-
-            if let Some(content_node) = self.content_ref.get() {
-                if let Some(submenu_node) = self.submenu_ref.get() {
-                    self.popper = Some(crate::create_popper(content_node, submenu_node, &opts));
-                }
-            }
         }
 
-        if let Some(popper) = &self.popper {
-            crate::update_popper(popper);
+        if let Err(err) = crate::widget::align::align_to(
+            &self.content_ref,
+            &self.submenu_ref,
+            self.align_options.clone(),
+        ) {
+            log::error!("could not position menu: {}", err.to_string());
         }
     }
 }
