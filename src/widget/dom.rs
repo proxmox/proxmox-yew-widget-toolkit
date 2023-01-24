4cee6168396990578bc60c3be08c002134676e9e
Dominik Csapak <d.csapak@proxmox.com>
2023-01-24 11:12:08 +0100

    widget: dom: add IntoHtmlElement trait and use it for element_direction_rtl
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- src/widget/dom.rs
+++ src/widget/dom.rs
@@ -1,14 +1,52 @@
 //! DOM helpers.
 
+use web_sys::Node;
 use yew::prelude::*;
+
+/// A Trait to convert structs into HtmlElement when possible
+pub trait IntoHtmlElement {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement>;
+}
+
+impl IntoHtmlElement for &NodeRef {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement> {
+        self.cast::<web_sys::HtmlElement>()
+    }
+}
+
+impl IntoHtmlElement for NodeRef {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement> {
+        (&self).into_html_element()
+    }
+}
+
+impl IntoHtmlElement for wasm_bindgen::JsValue {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement> {
+        match self.try_into() {
+            Ok(el) => Some(el),
+            Err(_) => None,
+        }
+    }
+}
+
+impl IntoHtmlElement for Node {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement> {
+        std::convert::Into::<wasm_bindgen::JsValue>::into(self).into_html_element()
+    }
+}
+
+impl IntoHtmlElement for web_sys::HtmlElement {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement> {
+        Some(self)
+    }
+}
 
 /// Detect if CSS `direction` property is set to `rtl` (left-to-right mode).
 ///
 /// Uses `getComputedStyle()` to get the inherited CSS value. Simply returns
 /// [None] on error.
-pub fn element_direction_rtl(node_ref: &NodeRef) -> Option<bool> {
-
-    let el = match node_ref.cast::<web_sys::HtmlElement>() {
+pub fn element_direction_rtl<T: IntoHtmlElement>(node: T) -> Option<bool> {
+    let el = match node.into_html_element() {
         Some(el) => el,
         None => return None,
     };
