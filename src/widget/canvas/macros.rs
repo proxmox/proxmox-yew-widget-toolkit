8ad441c8899440fb475db12b687485673f802fe8
Dietmar Maurer <dietmar@proxmox.com>
2023-02-09 11:50:04 +0100

    canvas: add methods to set the opacity

--- src/widget/canvas/macros.rs
+++ src/widget/canvas/macros.rs
@@ -33,5 +33,31 @@ macro_rules!impl_svg_presentation_attributes{
         pub fn set_fill(&mut self, fill: impl Into<AttrValue>) {
             self.set_attribute("fill", fill.into());
         }
+
+        /// Builder style method to set the object opacity.
+        pub fn opacity(mut self, opacity: f64) -> Self {
+            self.set_opacity(opacity);
+            self
+        }
+
+        /// Method to set the object opacity.
+        ///
+        /// Opacity is the degree to which content behind an element
+        /// is hidden, and is the opposite of transparency. Value are
+        /// between 0 (invisible) and 1.0 (intransparent).
+        pub fn set_opacity(&mut self, opacity: f64) {
+            self.set_attribute("opacity", opacity.to_string());
+        }
+
+        /// Builder style method to set the stroke opacity.
+        pub fn stroke_opacity(mut self, opacity: f64) -> Self {
+            self.set_stroke_opacity(opacity);
+            self
+        }
+
+        /// Method to set the stroke opacity.
+        pub fn set_stroke_opacity(&mut self, opacity: f64) {
+            self.set_attribute("stroke-opacity", opacity.to_string());
+        }
     }
 }
