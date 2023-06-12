a0f45348af057b5f3e560319bb992722f6f1fd52
Dietmar Maurer <dietmar@proxmox.com>
2023-06-12 13:26:22 +0200

    Scafold: add more properties

--- src/touch/scaffold/mod.rs
+++ src/touch/scaffold/mod.rs
@@ -2,9 +2,12 @@ use std::rc::Rc;
 
 use yew::prelude::*;
 use yew::virtual_dom::{Key, VComp, VNode};
+use yew::html::IntoPropValue;
 
-use crate::widget::Column;
+use crate::widget::{Column, Container};
 use crate::props::{ContainerBuilder, WidgetBuilder};
+
+use super::NavigationBar;
 
 /// Implements the basic Material Design visual layout structure.
 #[derive(Properties, Clone, PartialEq)]
@@ -12,8 +15,17 @@ pub struct Scaffold {
     /// The yew component key.
     pub key: Option<Key>,
 
-    /// The primary content displayed below the app bar.
-    pub body: Option<Html>,
+    /// The top application bar.
+    pub application_bar: Option<VNode>,
+
+    /// The primary content displayed below the application bar.
+    pub body: Option<VNode>,
+
+    /// The bottom navigation bar.
+    pub navigation_bar: Option<NavigationBar>,
+
+    /// Favorite action button.
+    pub favorite_action_button: Option<VNode>,
 }
 
 impl Scaffold {
@@ -23,8 +35,23 @@ impl Scaffold {
         yew::props!(Self {})
     }
 
+    pub fn application_bar(mut self, app_bar: impl Into<VNode>) -> Self {
+        self.application_bar = Some(app_bar.into());
+        self
+    }
+
     pub fn body(mut self, body: impl Into<VNode>) -> Self {
         self.body = Some(body.into());
+        self
+    }
+
+    pub fn navigation_bar(mut self, nav_bar: impl IntoPropValue<Option<NavigationBar>>) -> Self {
+        self.navigation_bar = nav_bar.into_prop_value();
+        self
+    }
+
+    pub fn favorite_action_button(mut self, fav: impl Into<VNode>) -> Self {
+        self.favorite_action_button = Some(fav.into());
         self
     }
 }
@@ -45,9 +72,28 @@ impl Component for PwtScaffold {
     fn view(&self, ctx: &Context<Self>) -> Html {
         let props = ctx.props();
 
+        let positioned_fab = props
+            .favorite_action_button
+            .clone()
+            .map(|fab| {
+                Container::new()
+                .class("pwt-position-absolute")
+                .class("pwt-right-2 pwt-bottom-2")
+                .with_child(fab)
+            });
+
+        let body = Column::new()
+            .class("pwt-position-relative")
+            .class("pwt-flex-fill")
+            .with_optional_child(props.body.clone())
+            .with_optional_child(positioned_fab);
+
+
         Column::new()
             .class("pwt-viewport")
-            .with_optional_child(props.body.clone())
+            .with_optional_child(props.application_bar.clone())
+            .with_child(body)
+            .with_optional_child(props.navigation_bar.clone())
             .into()
     }
 
