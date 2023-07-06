c741d11030518a235cd2e0326e5a8c1896551364
Dietmar Maurer <dietmar@proxmox.com>
2023-07-06 16:57:00 +0200

    remove popper.js js bindings

--- js-helper-module.js
+++ js-helper-module.js
@@ -4,31 +4,6 @@ function test_alert() {
     alert("This is a test!");
 }
 
-function create_popper(content, tooltip, opts) {
-
-    let setSameWidth = function(data) {
-	//console.log(data);
-	let state = data.state;
-	state.styles.popper["min-width"] = `${state.rects.reference.width}px`;
-	return state;
-    };
-
-    // Always use sameWidth? Maybe we need to make this optional
-    opts.modifiers.push({
-            name: "sameWidth",
-            enabled: true,
-            phase: "beforeWrite",
-            fn: setSameWidth,
-            requires: ["computeStyles"],
-    });
-
-    return Popper.createPopper(content, tooltip, opts);
-}
-
-function update_popper(popper) {
-    popper.update();
-}
-
 function show_modal_dialog(dialog) {
     dialog.showModal();
 }
@@ -39,8 +14,6 @@ function close_dialog(dialog) {
 
 export {
     test_alert,
-    create_popper,
-    update_popper,
     show_modal_dialog,
     close_dialog,
 };
