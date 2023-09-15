proxmox/proxmox-yew-widget-toolkit

commit aee917fa64d975f7388c9d8902c4f7f09a09cffc

use 'aria-disabled' instead of disabled for all widgets
this way, everything stays focusable, and we can still control the looks
and behaviour

for input fields we now have to set the 'readonly' flag, and disable the
events (pointer events are disabled via css)

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/widget/form/field2.rs
@@ -304,16 +304,18 @@ impl ManagedField for StandardField {
             Msg::Update(input.value())
         });
 
+        let disabled = props.input_props.disabled;
         let input: Html = Input::new()
             .node_ref(self.input_ref.clone())
             .with_input_props(&props.input_props)
             .class("pwt-flex-fill")
             .attribute("type", input_type)
+            .attribute("readonly", disabled.then_some(""))
             .attribute("value", value)
             .attribute("min", props.min.map(|v| v.to_string()))
             .attribute("max", props.max.map(|v| v.to_string()))
             .attribute("step", props.step.map(|v| v.to_string()))
-            .oninput(oninput)
+            .oninput(disabled.then_some(oninput))
             .into();
 
         let peek_icon =
@@ -342,6 +344,7 @@ impl ManagedField for StandardField {
             .listeners(&props.listeners)
             .class("pwt-input")
             .class(format!("pwt-input-type-{}", props.input_type))
+            .class(disabled.then_some("disabled"))
             .class("pwt-w-100")
             .class(if valid.is_ok() {
                 "is-valid"

src/props/field_std_props.rs
@@ -61,7 +61,10 @@ impl FieldStdProps {
         attr_map: &mut IndexMap<AttrValue, (AttrValue, ApplyAttributeAs)>,
     ) {
         if self.disabled {
-            attr_map.insert(AttrValue::Static("disabled"), (AttrValue::Static(""), ApplyAttributeAs::Attribute));
+            attr_map.insert(
+                AttrValue::Static("aria-disabled"),
+                (AttrValue::Static("true"), ApplyAttributeAs::Attribute),
+            );
         }
 
         if self.required {

src/widget/button.rs
@@ -243,7 +243,7 @@ impl Component for PwtButton {
             .tag("button")
             .class("pwt-button")
             .class(props.pressed.then(|| "pressed"))
-            .attribute("disabled", props.disabled.then(|| ""))
+            .attribute("aria-disabled", props.disabled.then(|| "true"))
             .attribute("autofocus", props.autofocus.then(|| ""))
             .attribute("aria-label", props.aria_label.clone())
             .attribute("tabindex", props.tabindex.map(|i| i.to_string()))

src/widget/menu/menu_checkbox.rs
@@ -313,7 +313,7 @@ impl Component for PwtMenuCheckbox {
         Container::new()
             .class("pwt-menu-item")
             .attribute("tabindex", (!disabled).then(|| "-1"))
-            .attribute("disabled", disabled.then(|| ""))
+            .attribute("aria-disabled", disabled.then(|| "true"))
             .attribute("role", if props.radio_group { "menuitemradio" } else { "menuitemcheckbox" })
             .attribute("aria-checked", checked.then(|| "true"))
             .onclick(onclick)

src/widget/menu/menu_checkbox2.rs
@@ -250,7 +250,7 @@ impl ManagedField for MenuCheckboxField {
         Container::new()
             .class("pwt-menu-item")
             .attribute("tabindex", (!disabled).then(|| "-1"))
-            .attribute("disabled", disabled.then(|| ""))
+            .attribute("aria-disabled", disabled.then(|| "true"))
             .attribute(
                 "role",
                 if props.radio_group {

src/widget/menu/menu_item.rs
@@ -270,6 +270,7 @@ impl Component for PwtMenuItem {
             html! {<i role="none" class={arrow_class}/>}
         });
 
+        let disabled = props.disabled;
         Container::new()
             .node_ref(self.content_ref.clone())
             .class(if props.inside_menubar {
@@ -277,11 +278,8 @@ impl Component for PwtMenuItem {
             } else {
                 "pwt-menu-item"
             })
-            .attribute(
-                "tabindex",
-                (!(props.disabled || props.focusable)).then(|| "-1"),
-            )
-            .attribute("disabled", props.disabled.then(|| ""))
+            .attribute("tabindex", (!props.focusable).then(|| "-1"))
+            .attribute("aria-disabled", props.disabled.then(|| "true"))
             .attribute("role", "menuitem")
             .attribute("aria-haspopup", has_submenu.then(|| "true"))
             .attribute(
@@ -292,7 +290,7 @@ impl Component for PwtMenuItem {
             .with_child(html! {<span class="pwt-flex-fill">{props.text.clone()}</span>})
             .with_optional_child(arrow)
             .with_optional_child(submenu)
-            .onkeydown({
+            .onkeydown((!disabled).then_some({
                 let link = ctx.link().clone();
                 move |event: KeyboardEvent| match event.key().as_str() {
                     "Enter" | " " => {
@@ -304,15 +302,15 @@ impl Component for PwtMenuItem {
                     }
                     _ => {}
                 }
-            })
-            .onclick({
+            }))
+            .onclick((!disabled).then_some({
                 let link = ctx.link().clone();
                 move |_| {
                     if !has_submenu {
                         link.send_message(Msg::Select)
                     };
                 }
-            })
+            }))
             .into()
     }
 

commit 87da58be82e64581350f00f09602f65af09e2bf9

form/field2: reverse oninput logic
we want to have the input callback when the input is *not* disabledo
so add it accordingly

we accidentally had it reversed

Fixes: aee917f ("use 'aria-disabled' instead of disabled for all widgets")
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/widget/form/field2.rs
@@ -315,7 +315,7 @@ impl ManagedField for StandardField {
             .attribute("min", props.min.map(|v| v.to_string()))
             .attribute("max", props.max.map(|v| v.to_string()))
             .attribute("step", props.step.map(|v| v.to_string()))
-            .oninput(disabled.then_some(oninput))
+            .oninput((!disabled).then_some(oninput))
             .into();
 
         let peek_icon =