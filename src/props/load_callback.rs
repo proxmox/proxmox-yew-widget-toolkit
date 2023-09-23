eb452c6f95d1625668dcf1801850b33a030b525c
Dietmar Maurer <dietmar@proxmox.com>
2023-09-23 16:06:45 +0200

    LoadCallback: cleanup

--- src/props/load_callback.rs
+++ src/props/load_callback.rs
@@ -1,14 +1,14 @@
-use std::rc::Rc;
+use std::cell::RefCell;
 use std::future::Future;
 use std::pin::Pin;
-use std::cell::RefCell;
+use std::rc::Rc;
 
 use anyhow::{bail, format_err, Error};
 use serde::de::DeserializeOwned;
 use serde_json::Value;
 
-use yew::AttrValue;
 use yew::html::IntoPropValue;
+use yew::AttrValue;
 
 /// Load Callback
 ///
@@ -27,12 +27,11 @@ use yew::html::IntoPropValue;
 /// change.
 
 pub struct LoadCallback<T> {
-    callback: Rc<dyn Fn() -> Pin<Box<dyn Future<Output=Result<T, Error>>>>>,
+    callback: Rc<dyn Fn() -> Pin<Box<dyn Future<Output = Result<T, Error>>>>>,
     url: Option<AttrValue>, // only used for change tracking
 }
 
 impl<T> LoadCallback<T> {
-
     pub fn new<F, R>(callback: F) -> Self
     where
         F: 'static + Fn() -> R,
@@ -71,7 +70,10 @@ impl<T> LoadCallback<T> {
 
 impl<T> Clone for LoadCallback<T> {
     fn clone(&self) -> Self {
-        Self { callback: Rc::clone(&self.callback), url: self.url.clone() }
+        Self {
+            callback: Rc::clone(&self.callback),
+            url: self.url.clone(),
+        }
     }
 }
 
@@ -82,6 +84,40 @@ impl<T> PartialEq for LoadCallback<T> {
         }
 
         Rc::ptr_eq(&self.callback, &other.callback)
+    }
+}
+
+impl<T: 'static + DeserializeOwned> From<String> for LoadCallback<T> {
+    fn from(url: String) -> Self {
+        LoadCallback::new({
+            let url = url.clone();
+            move || {
+                let url = url.clone();
+                let http_get = HTTP_GET.with(|cell| Rc::clone(&cell.borrow()));
+                async move {
+                    let value = http_get(url.to_string()).await?;
+                    let data = serde_json::from_value(value)?;
+                    Ok(data)
+                }
+            }
+        })
+        .url(url)
+    }
+}
+
+impl<T: 'static, F, R, P> From<(F, P)> for LoadCallback<T>
+where
+    P: Into<AttrValue>,
+    F: 'static + Fn(AttrValue) -> R,
+    R: 'static + Future<Output = Result<T, Error>>,
+{
+    fn from(tuple: (F, P)) -> Self {
+        let (callback, url) = (tuple.0, tuple.1.into());
+        let callback = {
+            let url = url.clone();
+            move || callback(url.clone())
+        };
+        LoadCallback::new(callback).url(url)
     }
 }
 
@@ -105,7 +141,7 @@ impl<T> IntoLoadCallback<T> for Option<LoadCallback<T>> {
 impl<T, F, R> IntoLoadCallback<T> for F
 where
     F: 'static + Fn() -> R,
-    R: 'static + Future<Output = Result<T, Error>>
+    R: 'static + Future<Output = Result<T, Error>>,
 {
     fn into_load_callback(self) -> Option<LoadCallback<T>> {
         Some(LoadCallback::new(self))
@@ -121,21 +157,7 @@ impl<T: 'static + DeserializeOwned> IntoLoadCallback<T> for &str {
 
 impl<T: 'static + DeserializeOwned> IntoLoadCallback<T> for String {
     fn into_load_callback(self) -> Option<LoadCallback<T>> {
-        let url = AttrValue::from(self);
-        Some(
-            LoadCallback::new({
-                let url = url.clone();
-                move || {
-                    let url = url.clone();
-                    let http_get = HTTP_GET.with(|cell| Rc::clone(&cell.borrow()));
-                    async move {
-                        let value = http_get(url.to_string()).await?;
-                        let data = serde_json::from_value(value)?;
-                        Ok(data)
-                    }
-                }
-            }).url(url)
-        )
+        Some(self.into())
     }
 }
 
@@ -149,7 +171,6 @@ impl<T: 'static + DeserializeOwned> IntoLoadCallback<T> for Option<String> {
     }
 }
 
-
 impl<T: 'static, F, R, P> IntoLoadCallback<T> for (F, P)
 where
     P: Into<AttrValue>,
@@ -157,16 +178,11 @@ where
     R: 'static + Future<Output = Result<T, Error>>,
 {
     fn into_load_callback(self) -> Option<LoadCallback<T>> {
-        let (callback, url) = (self.0, self.1.into());
-        let callback = {
-            let url = url.clone();
-            move || callback(url.clone())
-        };
-        Some(LoadCallback::new(callback).url(url))
+        Some((self.0, self.1.into()).into())
     }
 }
 
-thread_local!{
+thread_local! {
     static HTTP_GET: RefCell<Rc<dyn Send + Sync + Fn(String) -> Pin<Box<dyn Future<Output = Result<Value, Error>>>> >> = {
         RefCell::new(Rc::new(|url| Box::pin(http_get(url))))
     };
@@ -176,51 +192,48 @@ thread_local!{
 ///
 /// The default method expects a valid json response and simply
 /// deserializes the data using serde.
-pub fn set_http_get_method<F: 'static +  Future<Output = Result<Value, Error>>>(cb: fn(String) -> F) {
+pub fn set_http_get_method<F: 'static + Future<Output = Result<Value, Error>>>(
+    cb: fn(String) -> F,
+) {
     HTTP_GET.with(|cell| *cell.borrow_mut() = Rc::new(move |url| Box::pin(cb(url))));
 }
 
 async fn http_get(url: String) -> Result<Value, Error> {
-
     let mut init = web_sys::RequestInit::new();
     init.method("GET");
 
-    let js_headers = web_sys::Headers::new()
-        .map_err(|err| format_err!("{:?}", err))?;
+    let js_headers = web_sys::Headers::new().map_err(|err| format_err!("{:?}", err))?;
 
-    js_headers.append("content-type", "application/x-www-form-urlencoded")
+    js_headers
+        .append("content-type", "application/x-www-form-urlencoded")
         .map_err(|err| format_err!("{:?}", err))?;
 
     init.headers(&js_headers);
     let js_req = web_sys::Request::new_with_str_and_init(&url, &init)
         .map_err(|err| format_err!("{:?}", err))?;
 
-    let window = web_sys::window()
-        .ok_or_else(|| format_err!("unable to get window object"))?;
+    let window = web_sys::window().ok_or_else(|| format_err!("unable to get window object"))?;
 
     let promise = window.fetch_with_request(&js_req);
-    let js_fut =  wasm_bindgen_futures::JsFuture::from(promise);
-    let js_resp = js_fut.await
-        .map_err(|err| format_err!("{:?}", err))?;
+    let js_fut = wasm_bindgen_futures::JsFuture::from(promise);
+    let js_resp = js_fut.await.map_err(|err| format_err!("{:?}", err))?;
 
     let resp: web_sys::Response = js_resp.into();
 
-    let promise = resp.text()
-        .map_err(|err| format_err!("{:?}", err))?;
+    let promise = resp.text().map_err(|err| format_err!("{:?}", err))?;
 
-    let js_fut =  wasm_bindgen_futures::JsFuture::from(promise);
-    let body = js_fut.await
-        .map_err(|err| format_err!("{:?}", err))?;
+    let js_fut = wasm_bindgen_futures::JsFuture::from(promise);
+    let body = js_fut.await.map_err(|err| format_err!("{:?}", err))?;
 
-    let text = body.as_string()
+    let text = body
+        .as_string()
         .ok_or_else(|| format_err!("Got non-utf8-string response"))?;
 
     let data = if resp.ok() {
         if text.is_empty() {
             Value::Null
         } else {
-            serde_json::from_str(&text)
-                .map_err(|err| format_err!("invalid json: {}", err))?
+            serde_json::from_str(&text).map_err(|err| format_err!("invalid json: {}", err))?
         }
     } else {
         bail!("HTTP status {}: {}", resp.status(), resp.status_text());
