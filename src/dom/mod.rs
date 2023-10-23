9bb210f20b837f2f84b0785984fa8c9e8253902c
Dietmar Maurer <dietmar@proxmox.com>
2023-10-23 11:16:41 +0200

    src/dom/mod.rs: add missing file

---
+++ src/dom/mod.rs
@@ -0,0 +1,75 @@
+//! DOM helpers.
+
+mod number_format;
+pub use number_format::{format_float, parse_float, LocaleInfo};
+
+use web_sys::Node;
+use yew::prelude::*;
+
+/// A Trait to convert structs into HtmlElement when possible
+pub trait IntoHtmlElement {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement>;
+}
+
+impl IntoHtmlElement for &NodeRef {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement> {
+        self.cast::<web_sys::HtmlElement>()
+    }
+}
+
+impl IntoHtmlElement for NodeRef {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement> {
+        (&self).into_html_element()
+    }
+}
+
+impl IntoHtmlElement for wasm_bindgen::JsValue {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement> {
+        match self.try_into() {
+            Ok(el) => Some(el),
+            Err(_) => None,
+        }
+    }
+}
+
+impl IntoHtmlElement for Node {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement> {
+        std::convert::Into::<wasm_bindgen::JsValue>::into(self).into_html_element()
+    }
+}
+
+impl IntoHtmlElement for web_sys::HtmlElement {
+    fn into_html_element(self) -> Option<web_sys::HtmlElement> {
+        Some(self)
+    }
+}
+
+/// Detect if CSS `direction` property is set to `rtl` (left-to-right mode).
+///
+/// Uses `getComputedStyle()` to get the inherited CSS value. Simply returns
+/// [None] on error.
+pub fn element_direction_rtl<T: IntoHtmlElement>(node: T) -> Option<bool> {
+    let el = match node.into_html_element() {
+        Some(el) => el,
+        None => return None,
+    };
+
+    let window = web_sys::window().unwrap();
+    if let Ok(Some(style)) = window.get_computed_style(&el) {
+        if let Ok(direction) = style.get_property_value("direction") {
+            return Some(direction == "rtl");
+        }
+    }
+
+    None
+}
+
+/// Returns if the system prefers dark mode
+pub fn get_system_prefer_dark_mode() -> bool {
+    let window = web_sys::window().unwrap();
+    if let Ok(Some(list)) = window.match_media("(prefers-color-scheme: dark)") {
+        list.matches()
+    } else {
+        false
+    }
+}
