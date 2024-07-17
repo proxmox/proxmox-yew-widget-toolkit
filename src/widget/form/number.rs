d22332997cc412c0d90b4f075bbe8c365f9092fb
Dominik Csapak <d.csapak@proxmox.com>
2024-07-17 14:22:44 +0200

    widget: number field: let MIN/MAX be settable by step-up/down
    when stepping up or down for signed numbers (e.g. i64)
    the MIN/MAX value was not allowed. Fix the check to allow that.
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- src/widget/form/number.rs
+++ src/widget/form/number.rs
@@ -132,7 +132,7 @@ macro_rules! signed_number_impl {
             }
             fn step_down(&self, step: Option<Self>) -> Self {
                 let step = step.unwrap_or(1);
-                if *self > (<$T>::MIN + step) {
+                if *self >= (<$T>::MIN + step) {
                     self - step
                 } else {
                     *self
@@ -140,7 +140,7 @@ macro_rules! signed_number_impl {
             }
             fn step_up(&self, step: Option<Self>) -> Self {
                 let step = step.unwrap_or(1);
-                if *self < (<$T>::MAX - step) {
+                if *self <= (<$T>::MAX - step) {
                     self + step
                 } else {
                     *self
