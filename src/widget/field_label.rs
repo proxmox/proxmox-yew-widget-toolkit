proxmox/proxmox-yew-widget-toolkit

commit bd58deb3a7714e7e0d7dde5b4c7d5fff21718b2e

widget: input panel: use FieldLabel instead of just text
this simplifies the code here a bit, and makes it possible to give
each label separate classes and styles (such as alignment for example)

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/widget/input_panel.rs
@@ -8,8 +8,7 @@ use pwt_macros::{builder, widget};
 
 use crate::prelude::*;
 use crate::props::WidgetStyleBuilder;
-
-use super::Container;
+use crate::widget::{Container, FieldLabel};
 
 enum Position {
     Left,
@@ -176,7 +175,7 @@ impl InputPanel {
         &mut self,
         column: Position,
         advanced: bool,
-        label: impl IntoPropValue<AttrValue>,
+        label: impl Into<FieldLabel>,
         field: impl FieldBuilder,
     ) {
         let (label_column, row, field_class) = match column {
@@ -209,21 +208,14 @@ impl InputPanel {
         };
 
         let label_id = crate::widget::get_unique_element_id();
+        let label = label.into().id(label_id.clone());
         let class = classes!(
             format!("pwt-grid-column-{}", label_column),
             "pwt-align-self-center",
-            matches!(column, Position::Right).then_some("pwt-text-align-end"),
             field.is_disabled().then_some("pwt-label-disabled"),
         );
 
-        let label = label.into_prop_value();
-        let key = Key::from(format!("label_{label}"));
-
-        self.add_child(html! {
-            <label {key} id={label_id.clone()} {class} style={style.clone()}>
-                {label}
-            </label>
-        });
+        self.add_child(label.class(class).attribute("style", style.clone()));
 
         let name = field.as_input_props().name.clone();
         let field = field.label_id(label_id).into();
@@ -245,18 +237,14 @@ impl InputPanel {
     }
 
     /// Builder style method to add a field with label at the left column.
-    pub fn with_field(
-        mut self,
-        label: impl IntoPropValue<AttrValue>,
-        field: impl FieldBuilder,
-    ) -> Self {
+    pub fn with_field(mut self, label: impl Into<FieldLabel>, field: impl FieldBuilder) -> Self {
         self.add_field(false, label, field);
         self
     }
 
     pub fn with_advanced_field(
         mut self,
-        label: impl IntoPropValue<AttrValue>,
+        label: impl Into<FieldLabel>,
         field: impl FieldBuilder,
     ) -> Self {
         self.add_field(true, label, field);
@@ -267,7 +255,7 @@ impl InputPanel {
     pub fn add_field(
         &mut self,
         advanced: bool,
-        label: impl IntoPropValue<AttrValue>,
+        label: impl Into<FieldLabel>,
         field: impl FieldBuilder,
     ) {
         self.add_field_impl(Position::Left, advanced, label, field)
@@ -276,7 +264,7 @@ impl InputPanel {
     /// Builder style method to add a field with label at the right column.
     pub fn with_right_field(
         mut self,
-        label: impl IntoPropValue<AttrValue>,
+        label: impl Into<FieldLabel>,
         field: impl FieldBuilder,
     ) -> Self {
         self.add_right_field(false, label, field);
@@ -286,7 +274,7 @@ impl InputPanel {
     /// Builder style method to add a right column field in the advanced section.
     pub fn with_right_advanced_field(
         mut self,
-        label: impl IntoPropValue<AttrValue>,
+        label: impl Into<FieldLabel>,
         field: impl FieldBuilder,
     ) -> Self {
         self.add_right_field(true, label, field);
@@ -297,7 +285,7 @@ impl InputPanel {
     pub fn add_right_field(
         &mut self,
         advanced: bool,
-        label: impl IntoPropValue<AttrValue>,
+        label: impl Into<FieldLabel>,
         field: impl FieldBuilder,
     ) {
         self.add_field_impl(Position::Right, advanced, label, field)
@@ -306,7 +294,7 @@ impl InputPanel {
     /// Builder style method to add a large field spanning both columns.
     pub fn with_large_field(
         mut self,
-        label: impl IntoPropValue<AttrValue>,
+        label: impl Into<FieldLabel>,
         field: impl FieldBuilder,
     ) -> Self {
         self.add_large_field(false, label, field);
@@ -316,7 +304,7 @@ impl InputPanel {
     /// Builder style method to add a large field in the advanced section
     pub fn with_large_advanced_field(
         mut self,
-        label: impl IntoPropValue<AttrValue>,
+        label: impl Into<FieldLabel>,
         field: impl FieldBuilder,
     ) -> Self {
         self.add_large_field(true, label, field);
@@ -327,7 +315,7 @@ impl InputPanel {
     pub fn add_large_field(
         &mut self,
         advanced: bool,
-        label: impl IntoPropValue<AttrValue>,
+        label: impl Into<FieldLabel>,
         field: impl FieldBuilder,
     ) {
         self.add_field_impl(Position::Large, advanced, label, field)

commit 9a1b2ea9aab5f36f493a1b40947bcfa58d40015b

widget: input panel: fix key generation for labels
yews key mechanism does not work on markup level, but on component
level. So the attempt to auto-generate keys for FieldLabels in the view
method instead of the input panel was misguided, since that does not
actually give the label itself a key.

This was noticed on all input panels where we dynamically added/removed
fields, since then the whole form was reset because of the missing keys.
(E.g. the ACME DNS plugin panel)

Instead generate the label key like before in the input panel itself,
but only when the FieldLabel itself does not already have one set.
(so users can manually override that if they want)

Fixes: bd58deb (widget: input panel: use FieldLabel instead of just text)
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/widget/field_label.rs
@@ -34,7 +34,6 @@ impl Component for PwtFieldLabel {
         Container::from_tag("label")
             .with_std_props(&props.std_props)
             .listeners(&props.listeners)
-            .key(format!("label_{}", props.label))
             .with_child(props.label.clone())
             .into()
     }

src/widget/input_panel.rs
@@ -208,7 +208,10 @@ impl InputPanel {
         };
 
         let label_id = crate::widget::get_unique_element_id();
-        let label = label.into().id(label_id.clone());
+        let mut label: FieldLabel = label.into().id(label_id.clone());
+        if label.std_props.key.is_none() {
+            label.set_key(format!("label_{}", label.label));
+        }
         let class = classes!(
             format!("pwt-grid-column-{}", label_column),
             "pwt-align-self-center",