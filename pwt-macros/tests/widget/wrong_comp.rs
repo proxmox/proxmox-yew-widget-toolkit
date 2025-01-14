a84cc48fd6f26c21a1c5b1f6a6b1e204fb50be23
Shannon Sterz <s.sterz@proxmox.com>
2025-01-14 12:44:39 +0100

    macros: update wrong_comp test to adjust to new additional traits
    Signed-off-by: Shannon Sterz <s.sterz@proxmox.com>

--- pwt-macros/tests/widget/wrong_comp.rs
+++ pwt-macros/tests/widget/wrong_comp.rs
@@ -11,8 +11,27 @@ mod props {
     }
     pub trait CssPaddingBuilder {}
     pub trait CssMarginBuilder {}
+    pub trait AsCssStylesMut {
+        fn as_css_styles_mut(&mut self) -> &mut CssStyles;
+    }
+
+    /// Holds the CSS styles to set on elements
+    #[derive(Clone, Default, Debug, PartialEq)]
+    pub struct CssStyles {}
+
+    pub trait WidgetStyleBuilder {}
+
     pub trait WidgetBuilder: Sized {
         fn as_std_props_mut(&mut self) -> &mut WidgetStdProps;
+        fn as_std_props(&self) -> &WidgetStdProps;
+
+        fn class(mut self, class: impl Into<Classes>) -> Self {
+            self.add_class(class);
+            self
+        }
+        fn add_class(&mut self, class: impl Into<Classes>) {
+            self.as_std_props_mut().class.push(class);
+        }
     }
     pub trait AsClassesMut {
         fn as_classes_mut(&mut self) -> &mut Classes;
@@ -23,11 +42,11 @@ mod props {
     pub struct WidgetStdProps {
         pub key: Option<Key>,
         pub class: Classes,
+        pub styles: CssStyles,
     }
     #[derive(PartialEq, Default, Clone)]
     pub struct ListenersWrapper {}
 }
-
 use pwt_macros::widget;
 use yew::prelude::*;
 
