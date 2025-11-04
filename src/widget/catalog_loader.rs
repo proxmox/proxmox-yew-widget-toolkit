proxmox/proxmox-yew-widget-toolkit

commit 3a437d19c3bee74ec6ebdfef328e5e48c6f073f5

catalog loader: defer first content rendering after language is loaded
Currently, whenever the catalog loader tries to load a language, it
will empty out the content once, iow. render an empty body and then the
actual content again. This means that all sub components are recreated
and lose their state.

When changing between languages this is necessary, since otherwise
already rendered text will not be rerendered with the new language.

On first page load on the other hand, rendering once in the default
language and then immediately in the target language (even if it's also
the default language) does not make much sense, since
* most of the time the language catalog is rather small
  (e.g. pdm german mo file is ~75KiB), so the initial rendering is immediately
  lost and rerendered
* even if the language would take longer to load, displaying it for a
  relatively short time in the wrong language is not helpful.
* doing anything in a 'create' method of components that should only run
  once, cannot be done in a way that retains some info about it. For
  example the PMG mobile gui that tries to do some action from the GET
  parameters, but only once (including removing the GET parameters).
  That can work, but we can't show any popup/snackbar then since the
  language loaded in the meantime and we have no knowledge that we did
  such an action.

To fix this, render nothing until any language was at least loaded once.

On our older ExtJS GUIs, the server determines the language with a
cookie and the index has the link in the <head> section of the html.
This will block the javascript execution until that loaded, so this
solution here mimics the behavior on our ExtJS GUIs.

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/widget/catalog_loader.rs
@@ -79,6 +79,7 @@ pub struct PwtCatalogLoader {
     lang: String,
     last_url: String,
     state: LoadState,
+    loaded_once: bool,
 }
 
 impl Component for PwtCatalogLoader {
@@ -101,6 +102,7 @@ impl Component for PwtCatalogLoader {
             last_url: String::new(),
             state: LoadState::Idle,
             _observer,
+            loaded_once: false,
         }
     }
 
@@ -115,6 +117,7 @@ impl Component for PwtCatalogLoader {
                 } else {
                     log::warn!("could not load language info for '{}'", self.lang);
                 }
+                self.loaded_once = true;
                 self.state = LoadState::Idle;
                 true
             }
@@ -144,7 +147,7 @@ impl Component for PwtCatalogLoader {
     fn view(&self, ctx: &yew::Context<Self>) -> yew::Html {
         let props = ctx.props();
 
-        if matches!(self.state, LoadState::LoadFinished(_)) {
+        if !self.loaded_once || matches!(self.state, LoadState::LoadFinished(_)) {
             html! {}
         } else {
             html! {props.body.clone()}

commit 41cd56b003f662dc5dcd8e148d96849b004fe193

widget: catalog loader: fix behavior when no language is set
Recently, a change in the catalog loader was introduced to defer loading
of the page until after the language was loaded. This worked, except in
the case that no language was set at all, neither via a property nor via
the browser local storage.

When that occurred, there was no attempt to load the catalog but also
the indicator that we're finished with loading was not set, so the child
elements were never displayed.

To fix this, treat an empty language string the same as if it were the
'default_lang', which then triggers the initialization, etc. correctly.

Fixes:  3a437d1 (catalog loader: defer first content rendering after language is loaded)
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/widget/catalog_loader.rs
@@ -82,6 +82,16 @@ pub struct PwtCatalogLoader {
     loaded_once: bool,
 }
 
+impl PwtCatalogLoader {
+    fn set_language(&mut self, ctx: &yew::Context<Self>, lang: Option<String>, fallback: String) {
+        let mut lang = lang.unwrap_or(fallback);
+        if lang.is_empty() {
+            lang = ctx.props().default_lang.to_string();
+        }
+        self.lang = lang;
+    }
+}
+
 impl Component for PwtCatalogLoader {
     type Message = Msg;
     type Properties = CatalogLoader;
@@ -91,19 +101,17 @@ impl Component for PwtCatalogLoader {
 
         let _observer = LanguageObserver::new(ctx.link().callback(Msg::ChangeLanguage));
 
-        let lang = props
-            .lang
-            .clone()
-            .map(|l| l.to_string())
-            .unwrap_or(Language::load());
-
-        Self {
-            lang,
+        let mut this = Self {
+            lang: String::new(),
             last_url: String::new(),
             state: LoadState::Idle,
             _observer,
             loaded_once: false,
-        }
+        };
+
+        let lang = props.lang.clone().map(|l| l.to_string());
+        this.set_language(ctx, lang, Language::load());
+        this
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
@@ -126,7 +134,7 @@ impl Component for PwtCatalogLoader {
                 true
             }
             Msg::ChangeLanguage(lang) => {
-                self.lang = props.lang.clone().map(|l| l.to_string()).unwrap_or(lang);
+                self.set_language(ctx, props.lang.clone().map(|l| l.to_string()), lang);
                 true
             }
         }
@@ -135,11 +143,8 @@ impl Component for PwtCatalogLoader {
     fn changed(&mut self, ctx: &Context<Self>, old_props: &Self::Properties) -> bool {
         let props = ctx.props();
         if props.lang != old_props.lang {
-            self.lang = props
-                .lang
-                .clone()
-                .map(|l| l.to_string())
-                .unwrap_or(Language::load());
+            let lang = props.lang.clone().map(|l| l.to_string());
+            self.set_language(ctx, lang, Language::load());
         }
         true
     }
@@ -158,23 +163,20 @@ impl Component for PwtCatalogLoader {
         let props = ctx.props();
         match &self.state {
             LoadState::Idle => {
-                if !self.lang.is_empty() {
-                    let url = props.lang_to_url(&self.lang);
-                    if self.last_url != url {
-                        self.state = LoadState::Loading;
-                        let link = ctx.link().clone();
+                let url = props.lang_to_url(&self.lang);
 
-                        if self.lang == props.default_lang {
-                            crate::init_i18n(Catalog::empty());
+                if self.last_url != url {
+                    self.state = LoadState::Loading;
+                    let link = ctx.link().clone();
+
+                    if self.lang == props.default_lang {
+                        crate::init_i18n(Catalog::empty());
+                        link.send_message(Msg::LoadFinished(url));
+                    } else {
+                        crate::init_i18n_from_url(&url, move |url| {
                             link.send_message(Msg::LoadFinished(url));
-                        } else {
-                            crate::init_i18n_from_url(&url, move |url| {
-                                link.send_message(Msg::LoadFinished(url));
-                            });
-                        }
+                        });
                     }
-                } else {
-                    crate::init_i18n(Catalog::empty());
                 }
             }
             LoadState::Loading => { /* wait until loaded */ }