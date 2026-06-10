proxmox/proxmox-yew-widget-toolkit

commit ae9c35e531d2739a6f8d1527500056b0b2dd1a95

touch: slidable: improve action interface
usually when a widget has the capability to add children, a `with_child`
and `add_child` is provided. Do that here too for the left and right
actions.

This way a user does not have to wrap their actions in a row manually,
but can simply call 'with_left_action' multiple times.

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20260609133742.3249328-2-d.csapak@proxmox.com

src/touch/slidable/mod.rs
@@ -33,13 +33,23 @@ use pwt_macros::widget;
 pub struct Slidable {
     content: VNode,
 
-    /// Widget displayed on the left side (below the slider).
+    /// Widget displayed on the left side (below the slider). Takes precedence over
+    /// `left_action_list` and `with/add_left_action`.
     #[prop_or_default]
     pub left_actions: Option<VNode>,
 
-    /// Widget displayed on the right side (below the slider).
+    /// Widget displayed on the right side (below the slider). Takes precedence over
+    /// `right_action_list` and `with/add_right_action`.
     #[prop_or_default]
     pub right_actions: Option<VNode>,
+
+    #[prop_or_default]
+    /// A list of left hand side slidable actions.
+    pub left_action_list: Vec<SlidableAction>,
+
+    #[prop_or_default]
+    /// A list of right hand side slidable actions.
+    pub right_action_list: Vec<SlidableAction>,
 
     /// Dismiss callback.
     ///
@@ -99,6 +109,28 @@ impl Slidable {
     /// Method to set the `on_tap` callback.
     pub fn set_on_tap(&mut self, cb: impl IntoEventCallback<InputEvent>) {
         self.on_tap = cb.into_event_callback();
+    }
+
+    /// Method to add a left hand side action
+    pub fn add_left_action(&mut self, action: impl Into<SlidableAction>) {
+        self.left_action_list.push(action.into());
+    }
+
+    /// Builder style method to add a left hand side action
+    pub fn with_left_action(mut self, action: impl Into<SlidableAction>) -> Self {
+        self.add_left_action(action);
+        self
+    }
+
+    /// Method to add a right hand side action
+    pub fn add_right_action(&mut self, action: impl Into<SlidableAction>) {
+        self.right_action_list.push(action.into());
+    }
+
+    /// Builder style method to add a right hand side action
+    pub fn with_right_action(mut self, action: impl Into<SlidableAction>) -> Self {
+        self.add_right_action(action);
+        self
     }
 }
 
@@ -174,16 +206,23 @@ impl PwtSlidable {
 
     fn left_container(&self, ctx: &Context<Self>) -> Html {
         let props = ctx.props();
-        let actions = props.left_actions.clone();
+        let actions = match props.left_actions.clone() {
+            Some(actions) => vec![actions],
+            None => props
+                .left_action_list
+                .iter()
+                .map(|action| action.clone().into())
+                .collect(),
+        };
 
         Row::new()
             .class("pwt-w-100 pwt-h-100")
             .with_child(
-                Container::new()
+                Row::new()
                     .height(CssLength::Fraction(1.0))
                     .min_width(0)
                     .style("flex", "0 1 auto")
-                    .with_optional_child(actions)
+                    .children(actions)
                     .into_html_with_ref(self.left_action_ref.clone()),
             )
             .with_child(html! {<div style="flex: 1 1 auto;"></div>})
@@ -192,17 +231,24 @@ impl PwtSlidable {
 
     fn right_container(&self, ctx: &Context<Self>) -> Html {
         let props = ctx.props();
-        let actions = props.right_actions.clone();
+        let actions = match props.right_actions.clone() {
+            Some(action) => vec![action],
+            None => props
+                .right_action_list
+                .iter()
+                .map(|action| action.clone().into())
+                .collect(),
+        };
 
         Row::new()
             .class("pwt-w-100 pwt-h-100")
             .with_child(html! {<div style="flex: 1 1 auto;"></div>})
             .with_child(
-                Container::new()
+                Row::new()
                     .height(CssLength::Fraction(1.0))
                     .min_width(0)
                     .style("flex", "0 1 auto")
-                    .with_optional_child(actions)
+                    .children(actions)
                     .into_html_with_ref(self.right_action_ref.clone()),
             )
             .into_html_with_ref(self.right_ref.clone())

commit 06d858f455dbc1993d622f96a7a5cdc8c40bd4d1

touch: slidable: restore non-flex markup for old `x_actions` interface
Currently the slidable is used with `left_actions`, where a `Row` gets
used when adding multiple actions. Since commit ae9c35e ("touch:
slidable: improve action interface") this results in two nested Rows
with flex layout, which triggers what is seemingly a browser bug in
Chrome/Chromium where none of the text is rendered until some layout
changes. (The exact cause/reason is yet to be determined.)

To restore the original behavior, check the length of the action list
and use a normal container when it's a single item, only setting flex
display and direction when there are multiple.

Fixes: ae9c35e ("touch: slidable: improve action interface")
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20260610123916.2651267-2-d.csapak@proxmox.com
[TL: reword subject and fix typos in the commit message]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/touch/slidable/mod.rs
@@ -218,8 +218,13 @@ impl PwtSlidable {
         Row::new()
             .class("pwt-w-100 pwt-h-100")
             .with_child(
-                Row::new()
+                Container::new()
                     .height(CssLength::Fraction(1.0))
+                    .class(if actions.len() > 1 {
+                        Some(classes!(css::FlexDirection::Row, css::Display::Flex))
+                    } else {
+                        None
+                    })
                     .min_width(0)
                     .style("flex", "0 1 auto")
                     .children(actions)
@@ -244,7 +249,12 @@ impl PwtSlidable {
             .class("pwt-w-100 pwt-h-100")
             .with_child(html! {<div style="flex: 1 1 auto;"></div>})
             .with_child(
-                Row::new()
+                Container::new()
+                    .class(if actions.len() > 1 {
+                        Some(classes!(css::FlexDirection::Row, css::Display::Flex))
+                    } else {
+                        None
+                    })
                     .height(CssLength::Fraction(1.0))
                     .min_width(0)
                     .style("flex", "0 1 auto")